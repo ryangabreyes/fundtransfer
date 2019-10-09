3356cc2439482d785c0f05cde43ca932307c2f05
ryangabreyes <ryan.reyes@pointwest.com.ph>
2019-10-09 19:22:21 +0800

    Moved in FundTransferApplication

--- src/main/java/com/gcash/FundtransferApplication.java
+++ src/main/java/com/gcash/FundtransferApplication.java
@@ -1,14 +1,17 @@
-package com.gcash.fundtransfer;
+package com.gcash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class FundtransferApplication {
 
 	public static void main(String[] args) {
-		//rachel comment test
+
 		SpringApplication.run(FundtransferApplication.class, args);
+
+
 	}
 
 }
