1c9f172cfb932ecb144b432cec8303685c0d6f22
ryangabreyes <ryan.reyes@pointwest.com.ph>
2019-10-09 19:02:47 +0800

    Added Endpoints for Add Schedule and Get Schedule

--- src/main/java/com/gcash/controller/FundTransferController.java
+++ src/main/java/com/gcash/controller/FundTransferController.java
@@ -4,7 +4,6 @@ import com.gcash.model.FundTransferRequest;
 import com.gcash.model.FundTransferResponse;
 import com.gcash.model.ScheduleRequest;
 import com.gcash.model.ScheduleResponse;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FundTransferController {
 
-    @PostMapping(path="/addSchedule")
+    @PostMapping("/addSchedule")
     public FundTransferResponse addSchedule(@RequestBody FundTransferRequest request) {
         FundTransferResponse response = new FundTransferResponse();
+
+        response.setBank_code(request.getBank_code());
 
         return response;
     }
 
-    @PostMapping(path="/getSchedule")
+    @PostMapping("/getSchedule")
     public ScheduleResponse getSchedule(@RequestBody ScheduleRequest request) {
         ScheduleResponse response = new ScheduleResponse();
 
