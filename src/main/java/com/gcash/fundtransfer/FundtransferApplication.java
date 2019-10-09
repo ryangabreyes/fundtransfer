92935750d7df7d45cecee05fe9bb7afc8ee7b45f
ryangabreyes <ryan.reyes@pointwest.com.ph>
2019-10-09 15:23:27 +0800

    Initial Commit for Fundtransfer

---
+++ src/main/java/com/gcash/fundtransfer/FundtransferApplication.java
@@ -0,0 +1,14 @@
+package com.gcash.fundtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundtransferApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FundtransferApplication.class, args);
+	}
+
+}
